7f4ae5ac2ec07771dd7451a6f1d75ed699361a53
LivelyStork <krishna48705@sas.edu.sg>
2025-07-28 09:28:08 +0000

    Jul 28, 2025, 9:28 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -1,10 +1,17 @@
 /*
 Task 1: Add the setup() function
 */
+function setup() {
+  createCanvas(500,300);
+}
 
 function draw() {
+  background(220);
   // Challenge 1: An eye using ellipse() function
-
+// fill(83,34,58);
+// noStroke();
+// // noFill()
+// ellipse(100,100,50,50);
   // Challenge 2: A face using circle() function
 
   // Challenge 3: A house using rect() function
--- L03_patterns.js
+++ L03_patterns.js
@@ -1,10 +1,32 @@
 function setup() {
     createCanvas(600, 400);
     background(220);
-    noLoop(); // Stops continuous drawing
+    // noLoop(); // Stops continuous drawing
   }
-  
+  let colorValue = 0;
 function draw() {
+  // circle(75,100,100);
+  // circle(200,100,100);
+  // circle(325,100,100);
+  // circle(450,100,100);
+let spacing = 50;
+for (let i = 0; i < 5; i++) {
+  if(i%2==0) {
+    fill(0);
+  }
+  else {
+    fill(255);
+  }
+  // fill(i * 50);
+  // noStroke()
+  // fill(colorValue%255);
+  // fill(colorValue * 255)
+  circle(spacing + i * spacing,100,40);
+  // if (colorValue * 255 == 0)
+  // colorValue +=1;
+  // else
+  //   colorValue -=1
+}
   // Recap 1: Repeating Circles
 
   // Task 1: Colour Gradient
