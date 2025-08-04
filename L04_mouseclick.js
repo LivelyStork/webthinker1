4c6df2d490f679f7762c94319e71009616c6e30a
LivelyStork <krishna48705@sas.edu.sg>
2025-08-04 10:41:57 +0000

    Aug 4, 2025, 10:41 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -33,6 +33,18 @@ function draw() {
 
 // function mouseReleased() {
 //     shapeColor = 'blue';
+// fill(shapeColor);
+// circle(width/2, height/2, 100)
+
+// }
+
+// function mousePressed() {
+//     shapeColor = 'red';
+// }
+
+// function mouseReleased() {
+//     //shapeColor = 'blue';
+//     shapeColor = color(random(255), random(255), random(255));
 fill(shapeColor);
 circle(width/2, height/2, 100)
 
