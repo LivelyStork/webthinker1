f82c50e89f6e1988d6efbe5aff8e4f5dde074c78
LivelyStork <krishna48705@sas.edu.sg>
2025-08-04 10:17:42 +0000

    Aug 4, 2025, 10:17 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -5,15 +5,16 @@ function setup() {
   }
   
 function draw() {
-// let x = 50
-// let y = 50
-// let green = 0
+// let x = 50;
+// let y = 50;
+// let green = 0;
 // for (let i = 0; i < 5; i++) {
 //     fill(0,green,0)
 //     circle(x,y,30)
 //     x = x + 50
 //     y = y + 50
 //     green = green + 50
+
     for(let i=0; i<5; i++) {
         fill(0,i*50,0); //fill(r,g,b)
         circle(50 + i*60,50+i*60, 50);
