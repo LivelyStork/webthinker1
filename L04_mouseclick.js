3a5bf5d17336d7b0156280a5c4b3b2fc53957cc3
LivelyStork <krishna48705@sas.edu.sg>
2025-08-11 09:39:26 +0000

    Aug 11, 2025, 9:39 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -69,7 +69,7 @@ function setup() {
 function mouseDragged() {
     shapeColor = color(random(255), random(255), random(255));
     size = random(10, 50)
-    let mx = 
+    let mx = random(-30,30)
     fill(shapeColor)
     circle(mouseX, mouseY, size)
 }
