677e59c4d2850a9f86131f74e4b6b32141086482
LivelyStork <krishna48705@sas.edu.sg>
2025-08-18 10:20:23 +0000

    Aug 18, 2025, 10:20 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -52,6 +52,7 @@ function draw () {
     fill(shapeColor);
     circle(ball.x, height/2, 100);
     if(keyIsDown(LEFT_ARROW)){
+        if(key)
         ball.x-=5
     }
     if(keyIsDown(RIGHT_ARROW)){
