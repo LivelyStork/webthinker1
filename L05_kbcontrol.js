3e374ec010f8ba82017c8f9e1add54ac6a80aa50
LivelyStork <krishna48705@sas.edu.sg>
2025-08-18 10:36:22 +0000

    Aug 18, 2025, 10:36 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -110,7 +110,7 @@ function draw () {
             ball.y -= ball.speed;
         }
     }
-    if(keyIsDown(w)){
+    if(keyIsDown(W)){
         if(keyIsDown(SHIFT)){
             ball.y += ball.boost;
         }else{
