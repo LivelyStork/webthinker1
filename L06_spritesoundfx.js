96c8e7cf4dd061c577e84797a85fcff8cb5ff9fe
LivelyStork <krishna48705@sas.edu.sg>
2025-09-01 10:14:27 +0000

    Sep 1, 2025, 10:14 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,7 +1,7 @@
 let bgMusic, soundEffect, staticImage;
 
 function preload() {
-    bgMusic = loadSound('assets/bossaNova.mp3')
+    bgMusic = loadSound('assets/star_.mp3')
     soundEffect = loadSound('assets/pop.mp3')
     staticImage = loadImage('assets/pico-a.png')
 }
