c516d8475b3cd806dfe8196c0aa02bca193b059e
LivelyStork <krishna48705@sas.edu.sg>
2025-09-01 10:39:29 +0000

    Sep 1, 2025, 10:39 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -1,25 +1,41 @@
-let bgMusic, soundEffect, staticImage;
-let staticX = 0
-let staticY = 0
-let staticSpeed = 1
+//let bgMusic, soundEffect;
+let staticImage;
+let staticX = 0;
+let staticY = 0;
+let staticSpeed = 1;
 
 function preload() {
     // bgMusic = loadSound('assets/star_wars_theme_8_bit.mp3')
     // soundEffect = loadSound('assets/bossaNova.mp3')
-    staticImage = loadImage('assets/pico-a.png')
+    staticImage = loadImage('assets/pico-a.png');
 }
 
 function setup() {
-    createCanvas(300,300)
+    createCanvas(300,300);
 //     bgMusic.loop()
 }
 function draw() {
-    background('lightblue')
-    image(staticImage, staticX, staticY, 110, 133)
+    background('lightblue');
+    staticX = constrain(staticX,0,width-50);
+    staticY = constrain(staticY,0,height-50);
+    image(staticImage, staticX, staticY, 50, 50);
+    if(keyIsDown(LEFT_ARROW)) {
+        staticX -=staticSpeed
+    }
+    if(keyIsDown(RIGHT_ARROW)) {
+        staticX +=staticSpeed;
+    }
+    if(keyIsDown(DOWN_ARROW)) {
+        staticY +=staticSpeed
+    }
+    if(keyIsDown(UP_ARROW)) {
+        staticY -=staticSpeed
+    }
 }
 
-function keyPressed() {
+
+// function keyPressed() {
     // if (keyCode === 32) {
     //     soundEffect.play()
     // }
-}
+// } i can write what i want!!!! goofyah th
