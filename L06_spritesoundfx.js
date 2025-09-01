085bef9aa63d644a03ae8e73b3afce1ef4d45e81
LivelyStork <krishna48705@sas.edu.sg>
2025-09-01 10:43:36 +0000

    Sep 1, 2025, 10:43 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -6,7 +6,7 @@ let staticSpeed = 1;
 
 function preload() {
     // bgMusic = loadSound('assets/star_wars_theme_8_bit.mp3')
-    // soundEffect = loadSound('assets/bossaNova.mp3')
+    soundEffect = loadSound('assets/pop.mp3')
     staticImage = loadImage('assets/pico-a.png');
 }
 
