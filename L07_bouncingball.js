985084b6d63f9ad0c6aef6d5f271d56532759ead
LivelyStork <krishna48705@sas.edu.sg>
2025-09-08 10:53:16 +0000

    Sep 8, 2025, 10:53 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -3,43 +3,43 @@ function setup() {
     background(220);
 }
 
-let soundEffect, staticImage;
+// let soundEffect, staticImage;
 
-let staticX = 0;
-let staticY = 0;
-let staticSpeed = 5;
-let picoWidth = 50;
+// let staticX = 0;
+// let staticY = 0;
+// let staticSpeed = 5;
+// let picoWidth = 50;
 
-function preload() {
-    soundEffect = loadSound('assets/pop.mp3')
-    staticImage = loadImage('assets/pico-a.png');
-}
-function draw() {
-    background('lightblue');
-    staticX = constrain(staticX,0,width-picoWidth);
-    staticY = constrain(staticY,0,height-50);
-    image(staticImage, staticX, staticY, picoWidth, 50);
-    if(keyIsDown(LEFT_ARROW)) {
-        staticX -=staticSpeed;
-    }
-    if(keyIsDown(RIGHT_ARROW)) {
-        staticX +=staticSpeed;
-    }
-    if(keyIsDown(DOWN_ARROW)) {
-        staticY +=staticSpeed;
-    }
-    if(keyIsDown(UP_ARROW)) {
-        staticY -=staticSpeed;
-    }
-}
-function keyPressed() {
-    if(keyCode === 32) {
-        picoWidth = picoWidth/2;
-        soundEffect.play();
-    }
-}
-function keyReleased() {
-    if(keyCode === 32) {
-        picoWidth = picoWidth*2;
-    }
-}
+// function preload() {
+//     soundEffect = loadSound('assets/pop.mp3')
+//     staticImage = loadImage('assets/pico-a.png');
+// }
+// function draw() {
+//     background('lightblue');
+//     staticX = constrain(staticX,0,width-picoWidth);
+//     staticY = constrain(staticY,0,height-50);
+//     image(staticImage, staticX, staticY, picoWidth, 50);
+//     if(keyIsDown(LEFT_ARROW)) {
+//         staticX -=staticSpeed;
+//     }
+//     if(keyIsDown(RIGHT_ARROW)) {
+//         staticX +=staticSpeed;
+//     }
+//     if(keyIsDown(DOWN_ARROW)) {
+//         staticY +=staticSpeed;
+//     }
+//     if(keyIsDown(UP_ARROW)) {
+//         staticY -=staticSpeed;
+//     }
+// }
+// function keyPressed() {
+//     if(keyCode === 32) {
+//         picoWidth = picoWidth/2;
+//         soundEffect.play();
+//     }
+// }
+// function keyReleased() {
+//     if(keyCode === 32) {
+//         picoWidth = picoWidth*2;
+//     }
+// }
