489d46d11156de0800e6e91cd7c5c436a90d140a
LivelyStork <krishna48705@sas.edu.sg>
2025-09-15 10:53:00 +0000

    Sep 15, 2025, 10:53 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -8,6 +8,7 @@ function setup() {
 }
 let favFood = ["Rice", "Noodles", "Dumplings", "Sushi", "This is not accurate"]
 function draw() {
+    let valueY = height+50+food*50
     for(let food = 0; food<favFood.length; food++) {
         let valueY = height+50+food*50
         text("My Favorite Food:", 50, height)
