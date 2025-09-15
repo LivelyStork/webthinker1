bb66eef179171f9b9fdcaac2b9d9c66ceca552d2
LivelyStork <krishna48705@sas.edu.sg>
2025-09-15 10:58:11 +0000

    Sep 15, 2025, 10:58 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -9,7 +9,7 @@ function setup() {
 let favFood = ["Rice", "Noodles", "Dumplings", "Sushi", "This is not accurate"]
 function draw() {
     background(220);
-    translate(width)
+    translate(width/2,valueY)
     for(let food = 0; food<favFood.length; food++) {
         let valueY = height+50+food*50
         valueY -=2*food
