6630f6ea5dd4c8cca349f47f8a9c84b720eb4fe2
LivelyStork <krishna48705@sas.edu.sg>
2025-09-15 10:44:02 +0000

    Sep 15, 2025, 10:44 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -2,7 +2,7 @@ function setup() {
     createCanvas(600,400);
     background(220);
     textSize(36)
-    textAlign(CENTER)
+    textAlign(CENTER,BOTTOM)
     // text("My name is Utkarsh", 50,50);
     // text("My age is 12", 50,100);
     // text("My favorite activity is reading", 50,150);
