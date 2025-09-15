ff2bf5271fad13e667ffbf2621c9ef0ebf453c8a
LivelyStork <krishna48705@sas.edu.sg>
2025-09-15 10:44:34 +0000

    Sep 15, 2025, 10:44 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -2,7 +2,6 @@ function setup() {
     createCanvas(600,400);
     background(220);
     textSize(36)
-    textAlign(CENTER,)
     // text("My name is Utkarsh", 50,50);
     // text("My age is 12", 50,100);
     // text("My favorite activity is reading", 50,150);
@@ -10,7 +9,7 @@ function setup() {
 let favFood = ["Rice", "Noodles", "Dumplings", "Sushi", "This is not accurate"]
 let valueY = height+food*50
 function draw() {
-    text("My Favorite Food:",200, height)
+    text("My Favorite Food:",valueY -=200, height)
     for(let food = 0; food<favFood.length; food++) {
         text((food+1) + ". " + favFood[food], 50, valueY)
     }
