cc4168e6aa6fe5b0dafc66530b4266b3445a811a
LivelyStork <krishna48705@sas.edu.sg>
2025-09-22 09:53:29 +0000

    Sep 22, 2025, 9:53 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -15,7 +15,7 @@ function draw() {
 
     background(0); 
     // while (yPos == height - 600) {
-    if (yPos <= -200) {}
+    if (yPos <= -200) {
         translate(width/2,yPos); 
         scale(1,3); 
 
@@ -26,5 +26,5 @@ function draw() {
             text((i+1) + "." + favFood[i], 0, 50 + i*30); 
             //text((i+1) + "." + favFood[i], 50 + i*100, 80); 
         }
-    // }
+    }
 }
