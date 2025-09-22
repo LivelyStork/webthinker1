1ee91b46badf8336df06166b88e351cf777efc62
LivelyStork <krishna48705@sas.edu.sg>
2025-09-22 10:16:05 +0000

    Sep 22, 2025, 10:16 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,9 +1,12 @@
-let xPos = 
+let xPos = 0
 function setup () {
     createCanvas(600,400);
     background(220)
 }
 
 function draw () {
-    text("Bounce!", 0, 200)
+    text("Bounce!", xPos, 200)
+    if (xPos > 600) {
+        
+    }
 }
