2dd82f822e6f97859e783e14186b6f0537679d13
LivelyStork <krishna48705@sas.edu.sg>
2025-09-29 10:09:46 +0000

    Sep 29, 2025, 10:09 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -37,10 +37,10 @@ let countdown = 5;
 let interval;
 let intervalAfter;
 let bgmusic;
-let back
+let back;
 
 function preload() {
-    bgmusic = loadSound('assets/bossaNova.mp3')
+    bgmusic = loadSound('assets/bossaNova.mp3');
 }
 
 function setup () {
