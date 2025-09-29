463c5e2a20b3c0f40aad0702b73fa648b14e3609
LivelyStork <krishna48705@sas.edu.sg>
2025-09-29 10:24:54 +0000

    Sep 29, 2025, 10:24 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -113,7 +113,7 @@ function draw() {
     let s = second()
 
     let timeString = nf(h,2) + ":" + nf(m,2) + ":" + nf(s,2)
-    text(timeString, width/2, height/2)
+    text(timeString, width/2, 100)
 }
 
 function mousePressed() {
