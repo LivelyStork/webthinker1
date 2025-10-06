1444b9a52a31b1d6dc368d3ce918dbb4a83a93d0
LivelyStork <krishna48705@sas.edu.sg>
2025-10-06 10:16:37 +0000

    Oct 6, 2025, 10:16 AM

--- L10_name.js
+++ L10_name.js
@@ -90,7 +90,7 @@ let colorPicker;
     fill(0);
     textSize(24);
     textAlign(CENTER, CENTER);
-    text(userText, 300, );
+    text(userText, 300, 180);
 
     textSize(18);
     textAlign(LEFT, BOTTOM);
