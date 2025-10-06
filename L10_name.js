5bccb3c53e14521eea03f4ef755482821dfa62e8
LivelyStork <krishna48705@sas.edu.sg>
2025-10-06 10:04:26 +0000

    Oct 6, 2025, 10:04 AM

--- L10_name.js
+++ L10_name.js
@@ -18,7 +18,58 @@
 //     bgcolor = color(random(0,255), random(0,255), random(0,255))
 //  }
 
- let inputText
+//  let inputText
+//  let userText = "Enter text here"
+
+//  function setup() {
+//      createCanvas(600,400);
+
+//      inputText = createInput();
+//      inputText.position(width/2-100, height-80);
+//      inputText.input(userTextUpdate);
+
+//  }
+
+//  function draw() {
+//     background(220);
+
+//     fill(0);
+//     textSize(24);
+//     textAlign(CENTER, CENTER);
+//     text(userText, width/2, height/2);
+
+//     textSize(18);
+//     textAlign(LEFT, BOTTOM);
+//     text("Enter text here", 50, height-60);
+//  }
+
+//  function userTextUpdate() {
+    
+//     userText = this.value();
+ 
+//  }
+
+// let colorPicker;
+
+// function setup() {
+//     createCanvas(600,400);
+
+//     colorPicker = createColorPicker();
+//     colorPicker.position(300,300);
+// }
+
+// function draw() {
+//     background(colorPicker.value());
+//     fill(0);
+//     textSize(16);
+//     textAlign(LEFT);
+//     text("Pick a color: ", 50, 300+15);
+
+//     fill(255);
+//     rect(50,100,500,160,20)
+// }
+
+let inputText
  let userText = "Enter text here"
 
  function setup() {
