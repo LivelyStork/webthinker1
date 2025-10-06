1849b7037b93988b59db7709b76a20f01916e382
LivelyStork <krishna48705@sas.edu.sg>
2025-10-06 10:29:22 +0000

    Oct 6, 2025, 10:29 AM

--- L10_name.js
+++ L10_name.js
@@ -79,7 +79,9 @@ let colorPicker;
      inputText = createInput();
      inputText.position(width/2-100, height-80);
      inputText.input(userTextUpdate);
-     
+     inputAge = createInput();
+     inputText.position(width/2-100, height-80);
+     inputText.input(userTextUpdate);
      colorPicker = createColorPicker();
      colorPicker.position(300,300);
 
