f5fcd214af2b7bf2154154f48dfc4dfb6069bede
LivelyStork <krishna48705@sas.edu.sg>
2025-10-13 10:27:06 +0000

    Oct 13, 2025, 10:27 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -32,7 +32,8 @@ function setup() {
     createCanvas(600,400);
 
     input1 = createInput('Enter noun here');
-    input1.position(width/2 - input1.width/2, 50)
+    input1.position(width/2 - input1.width/2, 50);
+    button = 
 }
 
 function draw() {
