9801e77695917c5682b75fc15541bd75f449862d
LivelyStork <krishna48705@sas.edu.sg>
2025-10-13 09:33:51 +0000

    Oct 13, 2025, 9:33 AM

--- Revision2.js
+++ Revision2.js
@@ -1,15 +1,16 @@
-let arr = ["pizza", "apples", "juice", "tissues"];
+// let arr = ["pizza", "apples", "juice", "tissues"];
 
-function setup() {
-    createCanvas(600,400);
-    background('black')
-}
+// function setup() {
+//     createCanvas(600,400);
+//     background('black')
+// }
 
-function draw() {
-    fill('orange')
-    textAlign(CENTER)
-    textSize(24);
-    for(let i=0; i<arr.length; i++) {
-        text(arr[i], 300, i*50 + 70)
-    }
-}
+// function draw() {
+//     fill('orange')
+//     textAlign(CENTER)
+//     textSize(24);
+//     for(let i=0; i<arr.length; i++) {
+//         text(arr[i], 300, i*50 + 70)
+//     }
+// }
+
