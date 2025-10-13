e0fdd7a26ff62b1dd65ba88a044246630274df0b
LivelyStork <krishna48705@sas.edu.sg>
2025-10-13 10:01:00 +0000

    Oct 13, 2025, 10:01 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -1,27 +1,27 @@
-let colorTextPicker;
-let inputText;
-let userText;
+// let colorTextPicker;
+// let inputText;
+// let userText;
 
-function setup() {
-    createCanvas(600,400);
+// function setup() {
+//     createCanvas(600,400);
 
-    colorTextPicker = createColorPicker();
-    colorTextPicker.position(300,300)
-    inputText = createInput();
-    inputText.position(300, 200)
-    inputText.input(userTextUpdate);
-}
+//     colorTextPicker = createColorPicker();
+//     colorTextPicker.position(300,300)
+//     inputText = createInput();
+//     inputText.position(300, 200)
+//     inputText.input(userTextUpdate);
+// }
 
-function draw() {
-    background(220)
-    textSize(24);
-    fill('black')
-    text("Enter text here: ", 100, 200)
-    textSize(48);
-    fill(colorTextPicker.value())
-    text(userText, 300, 100)
-}
+// function draw() {
+//     background(220)
+//     textSize(24);
+//     fill('black')
+//     text("Enter text here: ", 100, 200)
+//     textSize(48);
+//     fill(colorTextPicker.value())
+//     text(userText, 300, 100)
+// }
 
-function userTextUpdate() {
-    userText = this.value();
-}
+// function userTextUpdate() {
+//     userText = this.value();
+// }
