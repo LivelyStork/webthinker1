e89df304dc9aa4d331002078ce78b01ecd718f11
LivelyStork <krishna48705@sas.edu.sg>
2025-10-13 10:27:37 +0000

    Oct 13, 2025, 10:27 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -34,7 +34,7 @@ function setup() {
     input1 = createInput('Enter noun here');
     input1.position(width/2 - input1.width/2, 50);
     button = createButton('Generate Story');
-    button
+    button.position
 }
 
 function draw() {
