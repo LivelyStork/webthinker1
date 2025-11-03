cd238e72e39c59019c6f94eec2ae75f810fcc266
LivelyStork <krishna48705@sas.edu.sg>
2025-11-03 10:55:48 +0000

    Nov 3, 2025, 10:55 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -19,7 +19,7 @@ function setup() {
 
     hiddenWord = random(words);
 
-    displayWord = hiddenWord[0].toUpperCase() + " " + "_".repeat
+    hiddenWord = hiddenWord[0].toUpperCase() + " " + "_".repeat
 }
 
 function draw() {
