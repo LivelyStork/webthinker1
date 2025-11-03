0399888d0af37092bf37aeedc90e443491bdede6
LivelyStork <krishna48705@sas.edu.sg>
2025-11-03 10:41:27 +0000

    Nov 3, 2025, 10:41 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -1,11 +1,14 @@
 let attempts = 0;
 let hiddenWord = "";
+let 
 
 function setup() {
     createCanvas(600,400);
 
     textBox = createInput();
     textBox.position(width/2 - textBox.width/2, 200);
+
+
 }
 
 function draw() {
