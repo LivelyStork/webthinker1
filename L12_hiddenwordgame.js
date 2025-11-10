2172cf5a53d07a4c5bf922d39a872ce6b3b472eb
LivelyStork <krishna48705@sas.edu.sg>
2025-11-10 10:08:15 +0000

    Nov 10, 2025, 10:08 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -47,7 +47,7 @@ function checkGuess() {
 function getCorrectLetters(guess, hiddenWord) {
     let correctLetters = "";
     for(let i=0;i<hiddenWord; i++) {
-        if(hiddenWord.includes(guess[i])) {
+        if(hiddenWord.includes(guess[i]) && !) {
             correctLetters += guess[i].toUpperCase();
         }
     }
