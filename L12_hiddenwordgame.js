bd6153923295da283a48240bc46fb0532d45220f
LivelyStork <krishna48705@sas.edu.sg>
2025-11-10 10:31:33 +0000

    Nov 10, 2025, 10:31 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -1,67 +1,67 @@
-// let attempts = 0;
-// let hiddenWord = "";
-// let textBox;
-// let button;
-// let words = ["apple", "mango", "grape", "peach", "melon", "table", "chair", "brick", "cloud", "storm", 
-//     "beach", "river", "flame", "grass", "skull", "horse", "laser", "brain", "plane", "train"];
-// let displayWord;
-// let message = "";
-// let hint = "";
+let attempts = 0;
+let hiddenWord = "";
+let textBox;
+let button;
+let words = ["apple", "mango", "grape", "peach", "melon", "table", "chair", "brick", "cloud", "storm", 
+    "beach", "river", "flame", "grass", "skull", "horse", "laser", "brain", "plane", "train"];
+let displayWord;
+let message = "";
+let hint = "";
 
-// function setup() {
-//     createCanvas(600,400);
+function setup() {
+    createCanvas(600,400);
 
-//     textBox = createInput();
-//     textBox.position(width/2 - textBox.width/2, 200);
+    textBox = createInput();
+    textBox.position(width/2 - textBox.width/2, 200);
 
-//     button = createButton('Guess');
-//     button.size(100,30);
-//     button.position(textBox.x + textBox.width + 30, 200);
-//     button.mousePressed(checkGuess);
+    button = createButton('Guess');
+    button.size(100,30);
+    button.position(textBox.x + textBox.width + 30, 200);
+    button.mousePressed(checkGuess);
 
-//     hiddenWord = random(words);
+    hiddenWord = random(words);
 
-//     hint = hiddenWord[0].toUpperCase() + " " + "_ ".repeat(hiddenWord.length-1)
+    hint = hiddenWord[0].toUpperCase() + " " + "_ ".repeat(hiddenWord.length-1)
+}
+
+function draw() {
+    background(220);
+    textAlign(CENTER,CENTER);
+    textSize(24);
+    text("Guess the word!", width/2, 50);
+    text("Attempts: " + attempts, width/2, 100);
+    text("Hint: " + hint, width/2, 150);
+    text(message, width/2, 300);
+}
+
+// function displayText() {
+//     displayWord = textBox.value();
 // }
 
-// function draw() {
-//     background(220);
-//     textAlign(CENTER,CENTER);
-//     textSize(24);
-//     text("Guess the word!", width/2, 50);
-//     text("Attempts: " + attempts, width/2, 100);
-//     text("Hint: " + hint, width/2, 150);
-//     text(message, width/2, 300);
-// }
+function checkGuess() {
+    attempts++;
 
-// // function displayText() {
-// //     displayWord = textBox.value();
-// // }
+    let guess = textBox.value().toLowerCase();
+    message = getCorrectLetters(guess, hiddenWord);
 
-// function checkGuess() {
-//     attempts++;
+    if(guess == hiddenWord) {
+        message = "You guessed it right! The word is " + hiddenWord;
+    } else if(guess.length != 5) {
+        message = "5 Letters Only!"
+    } else {
+        message = "Try Again. The letter(s) you got right is/are " + message;
+    }
+}
 
-//     let guess = textBox.value().toLowerCase();
-//     message = getCorrectLetters(guess, hiddenWord);
+function getCorrectLetters(guess, hiddenWord) {
+    let correctLetters = "";
+    for(let i=0;i<hiddenWord; i++) {
+        if(hiddenWord.includes(guess[i]) && !correctLetters.includes(guess[i].toUpperCase())) {
+            correctLetters += guess[i].toUpperCase();
+        }
+    }
 
-//     if(guess == hiddenWord) {
-//         message = "You guessed it right! The word is " + hiddenWord;
-//     } else if(guess.length != 5) {
-//         message = "5 Letters Only!"
-//     } else {
-//         message = "Try Again. The letter(s) you got right is/are " + message;
-//     }
-// }
-
-// function getCorrectLetters(guess, hiddenWord) {
-//     let correctLetters = "";
-//     for(let i=0;i<hiddenWord; i++) {
-//         if(hiddenWord.includes(guess[i]) && !correctLetters.includes(guess[i].toUpperCase())) {
-//             correctLetters += guess[i].toUpperCase();
-//         }
-//     }
-
-// }
+}
 ////////////////////////////////////////////////////////////
 
 // let attempts = 0;
