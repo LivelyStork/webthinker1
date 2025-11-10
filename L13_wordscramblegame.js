374250daaab67f51565a684bf981d8ad3e443b2c
LivelyStork <krishna48705@sas.edu.sg>
2025-11-10 10:48:10 +0000

    Nov 10, 2025, 10:48 AM

--- L13_wordscramblegame.js
+++ L13_wordscramblegame.js
@@ -1,10 +1,7 @@
-// write your codes here
-
 let words = [
     "elephant", "backpack", "keyboard", "hospital", "sunlight", "raincoat", "notebook", "shoulder", 
     "football", "bathroom", "sandwich", "airplane", "umbrella", "medicine", "chocolate", "software", 
     "pineapple", "furniture", "telephone", "lighthouse"];
-
 let attempts = 0;
 let hiddenWord = "";
 let textBox;
